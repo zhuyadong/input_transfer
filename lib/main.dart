a798843357ab5eb3f996fdc5b91a811e99482b42
zhuyadong <lordix@gmail.com>
2023-11-06 02:05:09 +0800

    SendInput ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,46 @@
 import 'dart:ffi';
 import 'dart:isolate';
+import 'package:ffi/ffi.dart';
+import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 import 'package:win32/win32.dart';
 
+ReceivePort mainReceivePort = ReceivePort();
+SendPort? mainSendPort;
 SendPort? hookSendPort;
-ReceivePort mainRecvPort = ReceivePort();
-
 void main() async {
-  await Isolate.spawn(hookIsolate, mainRecvPort.sendPort);
-  // hookSendPort = await mainRecvPort.single;
+  await Isolate.spawn(hookIsolate, mainReceivePort.sendPort);
   runApp(const MyApp());
 }
 
+const int cINPUT = 2;
+final pInputs = calloc<INPUT>(cINPUT);
 int lowLevelKeyboardProc(int nCode, int wParam, int lParam) {
   Pointer<KBDLLHOOKSTRUCT> ks = Pointer.fromAddress(lParam);
   if (ks.ref.flags == 128 || ks.ref.flags == 129) {
     switch(ks.ref.vkCode) {
       case 0x30 || 0x60:
-        print('--');
-      case 0x31 || 0x61:
-        print('++');
+        ZeroMemory(pInputs, sizeOf<INPUT>() * cINPUT);
+        mainSendPort?.send('${ks.ref.flags}');
+        pInputs[0].type = INPUT_KEYBOARD;
+        pInputs[0].ki.wVk = VK_LWIN;
+        pInputs[1].type = INPUT_KEYBOARD;
+        pInputs[1].ki.wVk = VK_LWIN;
+        pInputs[1].ki.dwFlags = KEYEVENTF_KEYUP;
+        // pInputs[2].type = INPUT_KEYBOARD;
+        // pInputs[2].ki.wVk = VK_HOME;
+        // pInputs[3].type = INPUT_KEYBOARD;
+        // pInputs[3].ki.wVk = VK_CONTROL;
+        // pInputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+        // pInputs[4].type = INPUT_KEYBOARD;
+        // pInputs[4].ki.wVk = VK_SHIFT;
+        // pInputs[4].ki.dwFlags = KEYEVENTF_KEYUP;
+        // pInputs[5].type = INPUT_KEYBOARD;
+        // pInputs[5].ki.wVk = VK_HOME;
+        // pInputs[5].ki.dwFlags = KEYEVENTF_KEYUP;
+        if (SendInput(cINPUT, pInputs, sizeOf<INPUT>()) != cINPUT) {
+          mainSendPort?.send('error SendInput');
+        }
     }
   }
 
@@ -28,9 +49,10 @@ int lowLevelKeyboardProc(int nCode, int wParam, int lParam) {
 
 void hookIsolate(SendPort sendPort){
   final receivePort = ReceivePort();
+  mainSendPort = sendPort;
   sendPort.send(receivePort.sendPort);
   SetWindowsHookEx(WH_KEYBOARD_LL, Pointer.fromFunction(lowLevelKeyboardProc, 0), GetModuleHandle(nullptr), 0);
-  final msg = wsalloc(1024) as Pointer<MSG>;
+  final msg = calloc<MSG>();
   while (GetMessage(msg, NULL, 0, 0) != 0) {
     TranslateMessage(msg);
     DispatchMessage(msg);
@@ -46,21 +68,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -71,79 +78,50 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
+class MyHomePageState extends State<MyHomePage> {
+  String _msg = 'empty';
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void eventLoop() async {
+    final events = StreamQueue(mainReceivePort);
+    hookSendPort = await events.next;
+    await for (final String msg in events.rest) {
+        setState(() {
+          _msg = msg;
+        });
+    }
+  }
+
+  MyHomePageState() {
+    eventLoop();
+  }
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              _msg,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
